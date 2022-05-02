b517e4541ef0d9e4a72f92905c916e0ea234a371
CleoBrown <gillmoynagh@hotmail.co.uk>
2022-05-02 17:42:46 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,5 @@
+puppys_name = input("What is your puppy's name?")
+puppys_age = input("what is your puppy's age?")
+age_dog_years = 7*int(puppys_age)
+print(puppys_name +  " is " + str(age_dog_years) + " in dog years") 
+
